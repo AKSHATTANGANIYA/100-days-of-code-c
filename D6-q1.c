37e594e6596f89d2a59a8bd95e0c6e5d8324f0a2
akshat26fe-ui <akshat26fe@gmail.com>
2026-08-15 22:13:28 +0530

    day 6

---
+++ D6-q1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter an integer = ");
+    scanf("%d",&a);
+    if(a%2==0){printf("%d is even",a);}
+    else{printf("%d is odd",a);}
+    return 0;
+}
+/*Write a program to input an integer and check whether it is even or odd using if–else*/
---
+++ D6-q2.c
@@ -0,0 +1,11 @@
+/*Write a program to input an integer and check whether it is positive, negative or zero using nested if–else*/
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter an integer = ");
+    scanf("%d",&a);
+    if (a>0){printf("%d is positive",a);}
+else if (a<0){printf("%d is negative",a);}
+else{printf("%d is zero",a);}
+return 0;
+}
