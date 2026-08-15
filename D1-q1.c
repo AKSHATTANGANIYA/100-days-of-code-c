eee7cb84754508caf64ed07c08b6f9e8351f5c44
akshat26fe-ui <akshat26fe@gmail.com>
2026-08-15 20:53:13 +0530

    test commit

---
+++ D1-q1.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main() {
+    float a, b, sum;
+    printf("Enter first number = ");
+    scanf("%f", &a);
+    printf("Enter second number = ");
+    scanf("%f", &b);
+    sum = a + b;
+    printf("Sum = %.2f", sum);
+    return 0;
+}
+/*Write a program to input two numbers and display their sum. */
