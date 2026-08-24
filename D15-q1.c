7567f6d0ad2a7b25a5819d6e3e662afae7a5152c
akshat26fe-ui <akshat26fe@gmail.com>
2026-08-24 23:21:03 +0530

    day 15

---
+++ D15-q1.c
@@ -0,0 +1,18 @@
+/*Write a program to calculate the factorial of a number*/
+#include<stdio.h>
+int main(){
+    int n, factorial = 1;
+    printf("Enter a number to calculate its factorial: ");
+    scanf("%d", &n);
+    
+    if(n < 0){
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        for(int i = 1; i <= n; i++){
+            factorial *= i; 
+        }
+        printf("Factorial of %d is: %d", n, factorial);
+    }
+    
+    return 0;
+}
