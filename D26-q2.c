3ce8bae76adcd58d85b3e2fead1d0c5f70c5eede
akshat26fe-ui <akshat26fe@gmail.com>
2026-09-04 21:57:24 +0530

    day 26

--- D26-q2.c
+++ D26-q2.c
@@ -21,15 +21,15 @@
  
 int main() {
     int i, j, k;
-    for(i=1;i<=3;i++) {
-        k=2*i-1;
-        for(j=0;j<k;j++)
+    for (i = 1; i <= 3; i++) {
+        k = 2 * i - 1;
+        for (j = 0; j < k; j++)
             printf("*\n");
         printf("\n");
- 
-    for(i=3-1;i>=1;i--) {
-        k=2*i-1;
-        for(j=0;j<k;j++)
+    }
+    for (i = 3 - 1; i >= 1; i--) {
+        k = 2 * i - 1;
+        for (j = 0; j < k; j++)
             printf("*\n");
         if (i > 1)
             printf("\n");
