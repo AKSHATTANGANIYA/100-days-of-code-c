bd43f58dee902118e2ba52d2c7db1e7ac98d0f8a
akshat26fe-ui <akshat26fe@gmail.com>
2026-09-14 18:39:27 +0530

    day 36

---
+++ D36-q1.c
@@ -0,0 +1,26 @@
+/*Read and print a matrix*/
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    printf("Enter number of rows and columns:\n");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[100][100];
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    printf("The matrix is:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d", matrix[i][j]);
+            if (j != cols - 1) printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ D36-q2.c
@@ -0,0 +1,22 @@
+/*Find the sum of all elements in a matrix.*/
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    printf("Enter number of rows and columns:\n");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[100][100];
+    int sum = 0; 
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+            sum += matrix[i][j];
+        }
+    }
+    printf("Sum of all elements in the matrix:\n");
+    printf("%d\n", sum);
+
+    return 0;
+}
