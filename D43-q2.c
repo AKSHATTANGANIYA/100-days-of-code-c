568e43694fab5549b87c2e2660679c8e86b7775f
AKSHATTANGANIYA <Akshat.33565@stu.upes.ac.in>
2026-09-21 01:54:50 +0530

    day 43

---
+++ D43-q2.c
@@ -0,0 +1,33 @@
+/*Check if a string is a palindrome*/
+#include <stdio.h>
+#include <string.h>
+
+int isPalindrome(char *str) {
+    int start = 0;
+    int end = strlen(str) - 1;
+
+    while (start < end) {
+        if (str[start] != str[end]) {
+            return 0;
+        }
+        start++;
+        end--;
+    }
+    return 1;
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+
+    if (isPalindrome(str)) {
+        printf("\"%s\" is a palindrome.\n", str);
+    } else {
+        printf("\"%s\" is not a palindrome.\n", str);
+    }
+
+    return 0;
+}
